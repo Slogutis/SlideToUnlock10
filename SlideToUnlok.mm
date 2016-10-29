814786224d212a00a5c6db33782e3bc78bd4a766
Abdullah Gabous <slogutis_dp@mohmal.com>
2016-10-29 18:44:25 +0300

    Add files via upload

---
+++ SlideToUnlok.mm
@@ -0,0 +1,6 @@
+SBLockScreenView
+- (void)setSlideToUnlockHidden:(BOOL)arg1 forRequester:(id)arg2;
+- (void)setSlideToUnlockBlurHidden:(BOOL)arg1 forRequester:(id)arg2;
+
+SBLockScreenViewController
+- (BOOL)shouldShowSlideToUnlockTextImmediately;
